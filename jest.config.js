e6a5c1e40185482f8158f9ac460b55a16a92434d
Ed Johnson <ed@neon.fish>
2023-05-25 09:39:23 +0100

    set max workers to 1 to speed up tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,13 @@
+
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  rootDir: "test"
+  rootDir: "test",
+  // globals: {
+  //   'ts-jest': {
+  //     isolatedModules: true
+  //   }
+  // },
+  maxWorkers: 1,
 };
